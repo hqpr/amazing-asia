e23d45478218514b428d029267bbb975425b76a6
hqpr <adubnyak@gmail.com>
2017-11-07 15:55:54 +0200

    Submit enquiry form

--- apps/enquiry/forms.py
+++ apps/enquiry/forms.py
@@ -8,3 +8,7 @@ class EnquiryForm(forms.ModelForm):
         model = Enquiry
         fields = ['full_name', 'email', 'phone', 'residence', 'property_name', 'room_type', 'arrival_date',
                   'num_of_nights', 'num_adults', 'num_children', 'num_infants']
+
+        widgets = {
+            'arrival_date': forms.TextInput(attrs={'class': 'form-control date-picker', 'placeholder': 'Arrival Date'}),
+        }
--- apps/enquiry/views.py
+++ apps/enquiry/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.views.generic import CreateView
 
 from .models import Enquiry
@@ -7,3 +8,6 @@ from .forms import EnquiryForm
 class AddEnquiryView(CreateView):
     model = Enquiry
     form_class = EnquiryForm
+
+    def get_success_url(self):
+        return reverse('ui:index')
